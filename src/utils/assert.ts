fc623f38945d844f6ddc7282eae208ef60cef06a
Jayson <jsoncp@proton.me>
2024-04-05 21:17:15 +1100

    Changed implem; Extended expect

--- src/utils/assert.ts
+++
@@ -1,9 +0,0 @@
-import { Locator, expect } from "@playwright/test";
-
-export class Assert {
-  async thatElementsAreVisible(elements: Locator[]): Promise<void> {
-    const results = await Promise.all(
-      elements.map((element) => expect(element).toBeVisible())
-    );
-  }
-}
