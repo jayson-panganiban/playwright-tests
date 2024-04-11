364ab2d1943666becf9e664ab582b63ae2fbb4c2
Jayson <jsoncp@proton.me>
2024-04-11 11:51:21 +1000

    refactored

--- src/utils/pageActions.ts
+++ src/utils/pageActions.ts
@@ -1,30 +1,28 @@
-import { Page } from "@playwright/test";
-import { Footer } from "../components/footer";
+import { Page } from '@playwright/test';
+import { Footer } from '../components/footer';
 
 export class PageActions {
-  readonly page: Page;
-  readonly footer: Footer;
+ private page: Page;
 
-  constructor(page: Page) {
+ constructor(page: Page) {
     this.page = page;
-  }
+ }
 
-  async scrollToBottom(page: Page): Promise<void> {
-    const footer = new Footer(page);
+ async scrollToBottom(): Promise<void> {
+    const footer = new Footer(this.page);
     try {
       while (true) {
-        await page.evaluate(() => {
+        await this.page.evaluate(() => {
           window.scrollTo(0, document.body.scrollHeight);
         });
-
-        await page.waitForTimeout(1500);
+        await this.page.waitForTimeout(1500);
         const stopCondition = await footer.text.isVisible();
         if (stopCondition) {
           break;
         }
       }
     } catch (error) {
-      console.error("Error while scrolling to bottom:", error);
+      console.error('Error while scrolling to bottom:', error);
     }
-  }
+ }
 }
